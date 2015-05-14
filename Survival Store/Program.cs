fc37e4e006d61c65770f05bfb82814a0cbaaede0
Sarah R <phoesarah@hotmail.com>
2015-05-14 16:20:34 -0500

    View by category working

--- Survival Store/Program.cs
+++ Survival Store/Program.cs
@@ -16,38 +16,31 @@ namespace Survival_Store
         {
             var listofallproducts = new List<Product>(); 
             var shoppingcart = new List<Product>();
-
+            var productlist = File.ReadAllLines("D:/Workprograms/Projects for class/Project3-NET041815/resources/New Text Document.txt");
             
             // for each line in excel document I want to create a product and send that to the list. listofallproducts
-           // Product productbyline = new Product();
-
-            var productlist = File.ReadAllLines("D:/Workprograms/Projects for class/Project3-NET041815/resources/New Text Document.txt");
+            
             foreach (var line in productlist)
             {
                 //split the line on tab
                var items = line.Split('\t');
                 //take the things it splits on each line, and create a product out of each index add that product to listofallproducts
                 //for (int i = 0; i < items.Length; i++)
-               // 
-                 //outside bounds of array... fix ? 
-                    var product = new Product(items[0],  items[1], items[2], Convert.ToDouble(items[3]), Convert.ToInt32(items[4]));
-                    listofallproducts.Add(product);
-                //}
-                //add product just created to the list ? 
-                
-            }
-//write what i'm getting so i know what it's doing-- YES IT IS ACTUALLY PUTTING THE STUFF IN THE LIST THANK THE LORD 
-          //  for (int i = 0; i < listofallproducts.Count; i++)
-         //  {
               
-        //       Console.WriteLine(listofallproducts[i].Category + listofallproducts[i].ItemNumber +
-         //          listofallproducts[i].ItemName + listofallproducts[i].Price + listofallproducts[i].NumberInStock);
-        //   }
+               var product = new Product(items[0],  items[1], items[2], Convert.ToDouble(items[3]), Convert.ToInt32(items[4]));
+                    listofallproducts.Add(product);
+            }
 
+//write what i'm getting so i know what it's doing-- YES IT IS ACTUALLY PUTTING THE STUFF IN THE LIST THANK THE LORD 
+//            for (int i = 0; i < listofallproducts.Count; i++)
+//          {
+              
+//             Console.WriteLine(listofallproducts[i].Category + listofallproducts[i].ItemNumber +
+//               listofallproducts[i].ItemName + listofallproducts[i].Price + listofallproducts[i].NumberInStock);
+//          }
 
-
-
-            Random walletamount = new Random(); 
+            double walletamount = 3000; //make into a random later
+            Console.WriteLine("Welcome, customer!");
             CallMenu();
             string input = Console.ReadLine();
             
@@ -67,7 +60,23 @@ namespace Survival_Store
                 case "P":
                 case "p":
                 {
-                    ViewProductsByCategory();
+                    Console.WriteLine("Our categories are as follows: ");
+                    Console.WriteLine("food");
+                    Console.WriteLine("tools");
+                    Console.WriteLine("first aid and medical");
+                    Console.WriteLine("warmth and shelter");
+                    Console.WriteLine("clothing");
+                    Console.WriteLine("cooking and fuel");
+                    Console.WriteLine("tools and supplies");
+                    Console.WriteLine("survival kits");
+                    Console.WriteLine("sanitation and hygiene");
+                    Console.WriteLine("emergency power");
+                    Console.WriteLine("light and communication");
+                    Console.WriteLine("backpacks");
+                    Console.WriteLine("Please enter the name of the category you would like to search for:");
+               
+               string category = Console.ReadLine().ToLower();
+                    ViewProductsByCategory(listofallproducts, category);
                     break; 
                 }
                     
@@ -92,21 +101,23 @@ namespace Survival_Store
                 }
 
             }
-               
+               CallMenu();
                 input = Console.ReadLine();
+                
             }
+            
 
             if (input == "q" || input == "Q")
             {
                 Environment.Exit(0);
             }
-           
+            
             
         }
 
        
 
-        private static void ViewWallet(Random walletamount)
+        private static void ViewWallet(Double walletamount)
         {
             throw new NotImplementedException();
         }
@@ -118,18 +129,43 @@ namespace Survival_Store
 
         private static void ViewAllProducts(List<Product> listofallproducts)
         {
-            throw new NotImplementedException();
+             for (int i = 0; i < listofallproducts.Count; i++)
+         {
+
+             Console.WriteLine("Product Number: " + listofallproducts[i].ItemNumber + " Category: " + listofallproducts[i].Category + " Product: " +
+               listofallproducts[i].ItemName + " Price: " + listofallproducts[i].Price + " We currently have " + listofallproducts[i].NumberInStock + " in stock.");
+          }
         }
 
-        private static void ViewProductsByCategory()
+        private static void ViewProductsByCategory(List<Product> listofallproducts, string input)
         {
-            throw new NotImplementedException();
+            if (input == "food" || input == "tools" || input == "first aid and medical" ||
+                input == "warmth and shelter" || input == "clothing" || input == "cooking and fuel" ||
+                input == "tools and supplies" || input == "survival kits" || input == "sanitation and hygiene" ||
+                input == "emergency power" || input == "light and communication" || input == "backpacks")
+            {
+
+
+                for (int i = 0; i < listofallproducts.Count; i++)
+                {
+                    if (listofallproducts[i].Category == input)
+
+                        Console.WriteLine("Product Number: " + listofallproducts[i].ItemNumber + " Category: " +
+                                          listofallproducts[i].Category + " Product: " +
+                                          listofallproducts[i].ItemName + " Price: " + listofallproducts[i].Price +
+                                          " We currently have " + listofallproducts[i].NumberInStock + " in stock.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("I'm sorry, We don't have that category, please check your spelling");
+            }
         }
 
        
         private static void CallMenu()
         {
-            Console.WriteLine("Welcome, customer!");
+            
             Console.WriteLine("If you would like to view all products, please type 'A'");
             Console.WriteLine("If you would like to view products by category, please type 'P'");
             Console.WriteLine("If you would like to view your shopping cart, please type 'C'");
